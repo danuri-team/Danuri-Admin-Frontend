eaf8d82b4d921a4b2002445175e492711bbedc8a
lioba00700 <lioba00700@naver.com>
2025-06-11 14:06:52 +0900

    feat :: API 비즈니스 로직 작성
    - 사용자 관리 관련 API
    - 아이템 관련 API
    - 사용 내역 관련 API
    - 관리자 인증 관련 API

---
+++ src/api/ItemAPI.ts
@@ -0,0 +1,64 @@
+import { PrivateAxios } from "./PrivateAxios"
+
+//아이템 생성
+export const postCreateItem = async ({name, totalQuantity, status}:{name:string, totalQuantity:string, status:string}) => {
+    try{
+        const res = await PrivateAxios.post('/admin/items', {
+            name,
+            total_quantity: totalQuantity,
+            status
+        })
+        return {data: res, pass: true}
+    }
+    catch(error){
+        return {data: error, pass:false}
+    }
+}
+
+//아이템 수정
+export const putUpdateItem = async ({itemId, name, totalQuantity, status}: {itemId:string, name:string, totalQuantity:string, status:string}) => {
+    try{
+        const res = await PrivateAxios.put(`/admin/items/${itemId}`, {
+            name,
+            total_quantity:totalQuantity,
+            status
+        })
+        return {data: res, pass: true}
+    }
+    catch(error){
+        return {data: error, pass:false}
+    }
+}
+
+//아이템 삭제
+export const deleteItem = async ({itemId}:{itemId:string}) => {
+    try{
+        const res = await PrivateAxios.delete(`/admin/items/${itemId}`);
+        return {data: res, pass: true}
+    }
+    catch(error){
+        return {data: error, pass:false}
+    }
+}
+
+//아이템 조회
+export const getSearchItem = async ({itemId}:{itemId:string}) => {
+    try{
+        const res = await PrivateAxios.get(`/admin/items/${itemId}`);
+        return {data: res, pass: true}
+    }
+    catch(error){
+        return {data: error, pass:false}
+    }
+}
+
+//사내 아이템 조회
+export const getSearchCompanyItem = async () => {
+    try{
+        const res = await PrivateAxios.get('/admin/items')
+        return {data: res, pass: true}
+    }
+    catch(error){
+        return {data: error, pass:false}
+    }
+}
