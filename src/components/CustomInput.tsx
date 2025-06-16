3aa203f2ef80ff50a39cafbf76c51ed2752d96f9
lioba00700 <lioba00700@naver.com>
2025-06-16 09:08:01 +0900

    fix :: 빌드 에러 수정
    - 사용하지 않는 import 제거

--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -1,8 +1,6 @@
 import { FaCircleCheck } from "react-icons/fa6";
 import { FaCircleExclamation } from "react-icons/fa6";
 
-import { useEffect } from "react"
-
 type CustomInputType = {
     label: string,
     value: string,
