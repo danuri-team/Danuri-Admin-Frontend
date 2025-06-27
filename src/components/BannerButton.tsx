e31a4d000b56aa58e38f2242d2d10da815e13dba
lioba00700 <lioba00700@naver.com>
2025-06-27 10:02:31 +0900

    chore :: 버그 제출 폼 연결

--- src/components/BannerButton.tsx
+++ src/components/BannerButton.tsx
@@ -2,7 +2,9 @@ import announceIcon from "../assets/announce-icon.png";
 
 const BannerButton = () => {
   return (
-    <button className="flex items-center bg-gray-100 rounded-xl w-[400px] p-[10px] justify-center mt-[40px] mb-[40px] justify-self-center cursor-pointer">
+    <button className="flex items-center bg-gray-100 rounded-xl w-[400px] p-[10px] justify-center mt-[40px] mb-[40px] justify-self-center cursor-pointer"
+      onClick={()=>window.location.href="https://docs.google.com/forms/d/e/1FAIpQLSdI9roa083JuemMBXG8gYZj51QCcG7jwm0VzV1J026vqEZTyw/viewform?usp=dialog"}
+    >
       <img src={announceIcon} alt="공지" />
       <span className="text-sm font-semibold ml-[10px]">
         버그 제보 혹은 피드백은 여기에 남겨주세요
