4acadb819751491dbdd0606ce033f3b342afcbe0
lioba00700 <lioba00700@naver.com>
2025-07-22 23:09:42 +0900

    chore :: 타입 가드 사용
    - 타입 단언 (as) 최소화

--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -4,46 +4,53 @@ import DatePicker from "react-datepicker";
 import { FaCircleCheck } from "react-icons/fa6";
 import { FaCircleExclamation } from "react-icons/fa6";
 
-type CustomInputType = 
-  | {
-      type?: string;    
-      isMust?: boolean;
-      label: string;
-      value: string;
-      onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-      valid?: boolean;
-      disabled?:boolean
-    }
-  | {
-      type: "time";
-      isMust?: boolean;
-      label: string;
-      value: Date | null;
-      onChange: (date: Date | null) => void;
-      valid?: boolean;
-      disabled?:boolean
+type BaseProps = {
+  isMust?:boolean;
+  label: string;
+  valid?: boolean;
+  disabled?:boolean;
+}
 
-  }
+type textProps = BaseProps & {
+  type?: Exclude<string, 'time'>;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
 
-const CustomInput = ({ isMust, type, label, value, onChange, valid, disabled }: CustomInputType) => {
+type timeProps = BaseProps & {
+  type: 'time';
+  value: Date | null;
+  onChange: (date: Date | null) => void;
+}
+
+type CustomInputType = textProps | timeProps
+
+const isTimeInput = (props:CustomInputType): props is timeProps => {
+  return props.type === 'time';
+}
+
+const CustomInput = (props: CustomInputType) => {
+  const {label, disabled, valid, isMust} = props;
   const [isFocus, setIsFocus] = useState<boolean>(false);
+  const isValid = !isTimeInput(props) && props.value.length > 0
+
   return (
     <div className="mb-[20px]">
       <p className="text-danuri-text mb-[7px] font-semibold text-sm">{label}{isMust && <span className="text-red-400"> *</span>}</p>
       <div
-        className={`${type!=='time' && (value as string).length > 0 && valid === false ? "border-red-400" : isFocus ? "border-blue-400" : "border-gray-200"}  flex border  rounded-xl p-[10px] w-full min-w-xs`}
+        className={`${isValid && valid === false ? "border-red-400" : isFocus ? "border-blue-400" : "border-gray-200"}  flex border  rounded-xl p-[10px] w-full min-w-xs`}
       >
         {
-          type==='time' ? (
+          isTimeInput(props) ? (
             <DatePicker
               className={`w-full outline-none placeholder:text-gray-300`}
               calendarClassName="border-gray-100 bg-blue-100 rounded-xl"
               placeholderText={`${label}을 선택해주세요.`}
-              selected={value as Date | null}
+              selected={props.value}
               onFocus={() => setIsFocus(true)}
               onBlur={() => setIsFocus(false)}
               locale={ko}
-              onChange={onChange as (date:Date|null)=>void}
+              onChange={props.onChange}
               dateFormat={`${"HH:mm"}`}
               showTimeSelect
               showTimeSelectOnly={true}
@@ -55,22 +62,22 @@ const CustomInput = ({ isMust, type, label, value, onChange, valid, disabled }: 
               className="outline-none w-full placeholder:text-gray-300"
               type={label === "비밀번호" ? "password" : "text"}
               placeholder={`${label}를 입력해주세요`}
-              value={value as string}
-              onChange={onChange as (e:React.ChangeEvent<HTMLInputElement>)=>void}
+              value={props.value}
+              onChange={props.onChange}
               onFocus={() => setIsFocus(true)}
               onBlur={() => setIsFocus(false)}
             />
           )
         }
-        {type!=='time' && (value as string).length > 0 && (valid === true || valid === false) && (
+        {isValid && (valid === true || valid === false) && (
           <button
-            className={`${type!=='time' && (value as string).length > 0 && valid === true ? "text-danuri-500" : "text-red-400"}`}
+            className={`${isValid && valid === true ? "text-danuri-500" : "text-red-400"}`}
           >
             {valid === false ? <FaCircleExclamation /> : valid === true && <FaCircleCheck />}
           </button>
         )}
       </div>
-      {type!=='time' && (value as string).length > 0 && valid === false && (
+      {isValid && valid === false && (
         <p className="text-xs mt-[10px] text-red-500">{`${label} 형식에 맞지 않습니다`}</p>
       )}
     </div>
