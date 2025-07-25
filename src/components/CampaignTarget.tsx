f0d84ff79e6c1fa275749580289c96341b7c54cf
lioba00700 <lioba00700@naver.com>
2025-07-26 02:45:09 +0900

    chore :: 캠페인 대상 필드 중복 입력 제한
    - 캠페인 관리 화면의 캠페인 대상 필드에서 같은 조건이 여러 번 입력되는 문제 해결

--- src/components/CampaignTarget.tsx
+++ src/components/CampaignTarget.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { IoChevronDown } from "react-icons/io5";
+import { toast } from "react-toastify";
 
 type TargetProps = {
     isMust: boolean;
@@ -17,21 +18,36 @@ const targetBlock = {
 }
 
 const CampaignTarget = ({isMust,label, value, onChange}:TargetProps) => {
-    const [tags, setTags] = useState<{key:string, value: string | null}[]>([]);
+    const [tags, setTags] = useState<{key:string, tagValue: string | null}[]>([]);
+
+    const addTargetTag = (key:string, tagValue: string[] | null) => {
+        if(tags.some((obj)=>obj.key === key)){
+            toast.error('이미 추가된 조건입니다.');
+            return;
+        }
+        setTags((prev)=>{
+            const tempValue = tagValue===null ? tagValue : tagValue[0];
+            return [...prev, {key, tagValue:tempValue} ]
+        })
+    }
 
     return(
         <div className="mb-[20px]">
             <p className="text-danuri-text mb-[7px] font-semibold text-sm">{label}{isMust && <span className="text-red-400"> *</span>}</p>
             <div
-                className={`border-gray-200 flex border rounded-xl p-[10px] w-full min-w-2xs min-h-[45px] overflow-x-scroll whitespace-nowrap`}
+                className={`border-gray-200 border rounded-xl p-[10px] w-full min-w-2xs min-h-[45px] overflow-x-auto whitespace-nowrap`}
             >
+                <div className="flex w-fit">
+
+                
                 {
                     tags.map((tagObj)=>{
                         const key = Object.entries(tagObj)[0][1];
-                        const value = Object.entries(tagObj)[1][1];
+                        const tagValue = Object.entries(tagObj)[1][1];
 
                         return (
                             <div 
+                                key={key}
                                 className="flex text-sm rounded-sm mr-[10px] overflow-hidden">
                                 <button
                                     className="cursor-pointer text-gray-500 bg-gray-100 p-[3px] pl-[10px] pr-[10px]"
@@ -39,10 +55,10 @@ const CampaignTarget = ({isMust,label, value, onChange}:TargetProps) => {
                                     {key}
                                 </button>
                                 {
-                                    value && (
+                                    tagValue && (
                                         <button
                                             className="cursor-pointer flex items-center gap-[3px] bg-blue-200 p-[3px] pl-[10px] pr-[10px]">
-                                            {value}
+                                            {tagValue}
                                             <IoChevronDown />
                                         </button>
                                     )
@@ -51,24 +67,20 @@ const CampaignTarget = ({isMust,label, value, onChange}:TargetProps) => {
                         )
                     })
                 }
-                
+                </div>
             </div>
             <div className="flex mt-[15px]">
                 {
-                    Object.entries(targetBlock).map(([key, value])=>(
+                    Object.entries(targetBlock).map(([key, tagValue])=>(
                         <button 
                             key={key} 
                             className="cursor-pointer text-sm text-gray-500 bg-gray-100 p-[3px] pl-[10px] pr-[10px] rounded-sm mr-[10px]"
-                            onClick={()=>setTags((prev)=>{
-                                const tempValue = value===null ? value : value[0];
-                                return [...prev, {key, value:tempValue} ]
-                            })}
+                            onClick={()=>addTargetTag(key, tagValue)}
                             >
                             {key}
                         </button>
                     ))
                 }
-
             </div>
         </div>
     )
