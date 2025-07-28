9f97f0f79a8b5a58a6c3a6fece6821ad12fae567
lioba00700 <lioba00700@naver.com>
2025-07-29 07:25:41 +0900

    chore :: import 주석 처리

--- src/components/joinForm/FieldOptions.tsx
+++ src/components/joinForm/FieldOptions.tsx
@@ -1,13 +1,13 @@
-import { IoTextOutline } from "react-icons/io5";
-import { FiPhone } from "react-icons/fi";
-import { FaRegSquareCheck } from "react-icons/fa6";
+// import { IoTextOutline } from "react-icons/io5";
+// import { FiPhone } from "react-icons/fi";
+// import { FaRegSquareCheck } from "react-icons/fa6";
 
 
-const options = [
-    {name: '객관식' , icon: <FaRegSquareCheck size={20}/>},
-    {name: '전화번호' , icon: <FaRegSquareCheck size={20}/>},
-    {name: '주관식' , icon: <FaRegSquareCheck size={20}/>},
-]
+// const options = [
+//     {name: '객관식' , icon: <FaRegSquareCheck size={20}/>},
+//     {name: '전화번호' , icon: <FaRegSquareCheck size={20}/>},
+//     {name: '주관식' , icon: <FaRegSquareCheck size={20}/>},
+// ]
 
 const FieldOptions = () => {
     return(
