ffff3b1b14d002fa6733dbabc1b09a21ba87a6ad
lioba <lioba@liobaui-MacBookAir.local>
2025-10-09 21:58:28 +0900

    feat :: 사용자 관리 헤더 api 연결

--- src/pages/management/UserPage.tsx
+++ src/pages/management/UserPage.tsx
@@ -49,15 +49,23 @@ const filterSelects: filterSelectType[] = [
   { id: "sex", type: "select", options: ["성별", "남", "여"] },
 ];
 
-const inputOption: Record<string, { label: string; key: string; type: ModalInputTypesType, initial?: string | number | Date, hide?: boolean }[]> = {
+const inputOption: Record<
+  string,
+  {
+    label: string;
+    key: string;
+    type: ModalInputTypesType;
+    initial?: string | number | Date;
+    hide?: boolean;
+  }[]
+> = {
   추가: [
-    { label: "회사 ID", key: "company_id", type: "text", hide:true },
     { label: "이름", key: "name", type: "text" },
     { label: "전화번호", key: "phone", type: "text" },
     { label: "성별", key: "sex", type: "option" },
   ],
   수정: [
-    { label: "사용자 ID", key: "id", type: "text", hide:true },
+    { label: "사용자 ID", key: "id", type: "text", hide: true },
     { label: "이름", key: "name", type: "text" },
     { label: "전화번호", key: "phone", type: "text" },
     { label: "성별", key: "sex", type: "option" },
@@ -80,7 +88,6 @@ const selectReducer = (state: SelectState, action: SelectAction) => {
       return initialSelectForm;
   }
 };
-
 
 //company id는 임의 값
 const modalSubmitFn: Record<string, ModalSubmitFn> = {
@@ -105,6 +112,7 @@ const modalSubmitFn: Record<string, ModalSubmitFn> = {
 const UserPage = () => {
   const navigate = useNavigate();
 
+  const [userTableHeader, setUserTableHeader] = useState<{ name: string; id: string }[]>([]);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [modalInputs, setModalInputs] = useState<
     { label: string; key: string; type: ModalInputTypesType }[] | null
@@ -112,73 +120,108 @@ const UserPage = () => {
   const [modalTitle, setModalTitle] = useState<string>("");
   const [tableData, setTableData] = useState<UsageData[] | null>(null);
   const [filterData, setFilterData] = useState<UsageData[] | null>(null);
-  
+
   const [isDeleteMode, setIsDeleteMode] = useState<boolean>(false);
-  const [selectedRowId, setSelectedRowId] = useState<string>('');
-  
+  const [selectedRowId, setSelectedRowId] = useState<string>("");
+
   const [selectForm, selectDispatch] = useReducer(selectReducer, initialSelectForm);
   const [isJoinForm, setIsJoinForm] = useState<boolean>(false);
-  
+
   useEffect(() => {
-    if (isModalOpen === true || modalTitle === '검색') return;
+    if (isModalOpen === true || modalTitle === "검색") return;
     setModalTitle("");
     const getTableData = async () => {
       const res = await getSearchCompanyUser();
       if (res.pass) {
-        setTableData(res.data.user_list);
+        const users = res.data.user_list.map(
+          (user: {
+            id: string;
+            phone: string;
+            sign_up_form_schema: string;
+            created_at: string;
+            updated_at: string;
+            usage_count: number;
+          }) => {
+            let str = user.sign_up_form_schema;
+            const arr = str.slice(1, str.length - 1).split(",");
+            const schema = Object.fromEntries(
+              arr.map((str) => {
+                const [key, value] = str.split(":");
+                return [key.trim(), value.trimStart()];
+              })
+            );
+            return {
+              ...user,
+              ...schema,
+            };
+          }
+        );
+        setTableData(users);
+        const headers = res.data.header_list.map((header: string) => ({
+          name: header,
+          id: header,
+        }));
+        setUserTableHeader([{ name: "ID", id: "id" }, ...headers]);
       }
     };
 
     getTableData();
   }, [isModalOpen, isDeleteMode, modalTitle]);
 
-  useEffect(()=>{
-    if(!tableData)return;
+  useEffect(() => {
+    if (!tableData) return;
     const filterTableData = tableData.filter((item) => {
-      const matchJoinDate = !selectForm.joinDate 
-        || (item.created_at as string).substring(0,10)===formatDatetoISOString(selectForm.joinDate as Date).substring(0,10)
-      const matchAge = selectForm.age === '나이대' || (selectForm.age === '고등학생' && item.age === 'HIGH') || (selectForm.age === '중학생' && item.age === 'MIDDLE');
-      const matchSex = selectForm.sex === '성별' || (selectForm.sex === '남' && item.sex === 'MALE') || (selectForm.sex === '여' && item.sex === 'FEMALE');
-      
-      return matchJoinDate && matchAge && matchSex
-    })
-    setFilterData(filterTableData)
-  },[selectForm, tableData])
+      const matchJoinDate =
+        !selectForm.joinDate ||
+        (item.created_at as string).substring(0, 10) ===
+          formatDatetoISOString(selectForm.joinDate as Date).substring(0, 10);
+      const matchAge =
+        selectForm.age === "나이대" ||
+        (selectForm.age === "고등학생" && item.age === "HIGH") ||
+        (selectForm.age === "중학생" && item.age === "MIDDLE");
+      const matchSex =
+        selectForm.sex === "성별" ||
+        (selectForm.sex === "남" && item.sex === "MALE") ||
+        (selectForm.sex === "여" && item.sex === "FEMALE");
 
-  useEffect(()=>{
+      return matchJoinDate && matchAge && matchSex;
+    });
+    setFilterData(filterTableData);
+  }, [selectForm, tableData]);
+
+  useEffect(() => {
     const getData = async () => {
       const res = await getJoinForm();
-      if(res.pass){
-        if(res.data.schema===""){
-          setIsJoinForm(false)        
-        }
-        else {
+      if (res.pass) {
+        if (res.data.schema === "") {
+          setIsJoinForm(false);
+        } else {
           setIsJoinForm(true);
         }
       }
-    }
-  getData();
-  },[])
+    };
+    getData();
+  }, []);
 
-  const changeSelectedRow = ({id}:{id:string}) => {
+  const changeSelectedRow = ({ id }: { id: string }) => {
     setSelectedRowId(id);
-  }
+  };
 
-  const searchTableData = (form:modalState) => {
-    if(!tableData)return;
-    const searchData = tableData.filter((item)=>{
+  const searchTableData = (form: modalState) => {
+    if (!tableData) return;
+    const searchData = tableData.filter((item) => {
       const searchName = !form.name || form.name === item.name;
       const searchPhone = !form.phone || form.phone === item.phone;
       const searchSex = !form.sex || form.sex === item.sex;
       const searchAge = !form.age || form.age === item.age;
 
       return searchName && searchPhone && searchSex && searchAge;
-    })
+    });
     setFilterData(searchData);
-  }
+  };
 
   const onClickTableButton = ({ value }: { value: string }) => {
-    if(value==='삭제'){
+    if (value === "삭제") {
       onClickDeleteButton();
       return;
     }
@@ -190,43 +233,40 @@ const UserPage = () => {
   };
 
   const onClickDeleteButton = async () => {
-    if(!isDeleteMode){
+    if (!isDeleteMode) {
       setIsDeleteMode(true);
-    }
-    else {
-      if(!selectedRowId){
-        toast.error('선택된 사용자가 없습니다.');
+    } else {
+      if (!selectedRowId) {
+        toast.error("선택된 사용자가 없습니다.");
         setIsDeleteMode(false);
         return;
       }
-      const res = await deleteUser({userId: selectedRowId});
-      if(res.pass){
-        toast.success('삭제되었습니다.');
+      const res = await deleteUser({ userId: selectedRowId });
+      if (res.pass) {
+        toast.success("삭제되었습니다.");
         setIsDeleteMode(false);
-      }
-      else {
-        toast.error('삭제에 실패했습니다.');
+      } else {
+        toast.error("삭제에 실패했습니다.");
       }
     }
-  }
+  };
 
-  const onClickTableRow = (row:UsageData) => {
-    setModalTitle('수정');
-    const addInitialInputs = inputOption['수정'].map((item) => {
+  const onClickTableRow = (row: UsageData) => {
+    setModalTitle("수정");
+    const addInitialInputs = inputOption["수정"].map((item) => {
       return {
         ...item,
-        initial: item.key==='itemId' ? row.id : row[item.key]
-      }
-    })
+        initial: item.key === "itemId" ? row.id : row[item.key],
+      };
+    });
     setModalInputs(addInitialInputs);
     setIsModalOpen(true);
-  }
+  };
 
   const onCloseModal = () => {
     setIsModalOpen(false);
     setModalInputs(null);
   };
-
 
   return (
     <div className="w-full">
@@ -253,16 +293,25 @@ const UserPage = () => {
           </div>
           <div className="flex gap-[10px]">
             <TableButton value="추가" onClick={() => onClickTableButton({ value: "추가" })} />
-            <TableButton value="검색" onClick={() => onClickTableButton({ value: "검색" })}/>
-            <TableButton value="삭제" onClick={() => onClickTableButton({ value: "삭제" })} isDeleteMode={isDeleteMode}/>
-          {
-            isJoinForm || (
-              <TableButton value="가입 폼 관리" onClick={() => navigate('/joinForm')}/>
-            )
-          }
+            <TableButton value="검색" onClick={() => onClickTableButton({ value: "검색" })} />
+            <TableButton
+              value="삭제"
+              onClick={() => onClickTableButton({ value: "삭제" })}
+              isDeleteMode={isDeleteMode}
+            />
+            {isJoinForm || (
+              <TableButton value="가입 폼 관리" onClick={() => navigate("/joinForm")} />
+            )}
           </div>
         </div>
-        <CustomTable header={tableHeader} data={filterData} rowUpdate={onClickTableRow} isDeleteMode={isDeleteMode} changeSelectedRow={changeSelectedRow} selectedRowId={selectedRowId} />
+        <CustomTable
+          header={userTableHeader}
+          data={filterData}
+          rowUpdate={onClickTableRow}
+          isDeleteMode={isDeleteMode}
+          changeSelectedRow={changeSelectedRow}
+          selectedRowId={selectedRowId}
+        />
       </div>
       {isModalOpen && (
         <Modal
@@ -270,7 +319,11 @@ const UserPage = () => {
           title={modalTitle}
           inputs={modalInputs}
           onClose={onCloseModal}
-          onSubmit={modalTitle!=='검색' ? modalSubmitFn[modalTitle] : (form:modalState)=>searchTableData(form)}
+          onSubmit={
+            modalTitle !== "검색"
+              ? modalSubmitFn[modalTitle]
+              : (form: modalState) => searchTableData(form)
+          }
         />
       )}
     </div>
